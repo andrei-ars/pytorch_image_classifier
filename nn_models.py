67e33046fc6d742b843d3f1da4373de1149aa472
Andrei <z@sp.sp>
2021-10-17 02:12:26 +0300

    transf

--- nn_models.py
+++ nn_models.py
@@ -6,7 +6,7 @@ from torchvision import models
 
 def get_resnet18_classifier(num_classes):
 
-    model_ft = models.resnet18(pretrained=True)
+    model_ft = models.resnet18(pretrained=False)
     num_ftrs = model_ft.fc.in_features
     model_ft.fc = nn.Linear(num_ftrs, num_classes)
     return model_ft
@@ -14,7 +14,7 @@ def get_resnet18_classifier(num_classes):
 
 def get_torchvision_model(output_size, pretrained=True):
 
-    model_ft = models.mobilenet_v2(pretrained=True)
+    model_ft = models.mobilenet_v2(pretrained=pretrained)
     num_ftrs = model_ft.classifier[1].in_features
     model_ft.classifier[1] = nn.Linear(num_ftrs, output_size)
     return model_ft
